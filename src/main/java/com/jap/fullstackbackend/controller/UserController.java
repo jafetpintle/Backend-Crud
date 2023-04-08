5b1f969792ebca1ec5d81117b7a4766cf3b2009c
jafet <jafetpintle@gmail.com>
2023-04-08 13:41:20 -0600

    User controller

---
+++ src/main/java/com/jap/fullstackbackend/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.jap.fullstackbackend.controller;
+
+
+import com.jap.fullstackbackend.model.User;
+import com.jap.fullstackbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+}
