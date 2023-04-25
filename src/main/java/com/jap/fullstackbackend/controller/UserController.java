34c9a63f5f879d0feb3ec4ebbc1b7c9fc097158e
jafet <jafetpintle@gmail.com>
2023-04-24 22:44:15 -0600

    Updating user data

--- src/main/java/com/jap/fullstackbackend/controller/UserController.java
+++ src/main/java/com/jap/fullstackbackend/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,5 +39,19 @@ public class UserController {
     User getUserById(@PathVariable Long id){
         return userRepository.findById(id)
                 .orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser ,@PathVariable Long id){
+        return userRepository.findById(id)
+                .map(user -> {
+                    if (newUser.getUsername()!= null)
+                        user.setUsername(newUser.getUsername());
+                    if (newUser.getEmail()!= null)
+                        user.setEmail(newUser.getEmail());
+                    if (newUser.getName()!= null)
+                        user.setName(newUser.getName());
+                    return userRepository.save(user);
+                }).orElseThrow(()-> new UserNotFoundException(id));
     }
 }
